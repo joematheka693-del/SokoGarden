9547d48e82fa34558dc4c5e83e5acfcc7b322017
joematheka693-del <joematheka@gmail.com>
2026-04-15 11:34:19 +0300

    Update

--- app/src/main/java/com/example/sokogarden/Signin.kt
+++ app/src/main/java/com/example/sokogarden/Signin.kt
@@ -1,10 +1,15 @@
 package com.example.sokogarden
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.loopj.android.http.RequestParams
 
 class Signin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,40 @@ class Signin : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
+//        Find the two editText, a button and Textview by use of their ids
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val signupBtn = findViewById<Button>(R.id.signinBtn)
+        val signuptxt = findViewById<TextView>(R.id.signuptxt)
+
+//        On the textview set on click listener so that it redirects to the signup activity
+        signuptxt.setOnClickListener {
+            val intent = Intent(applicationContext, Signup::class.java)
+            startActivity(intent)
+        }
+
+//        on click of the button sign in, we need to interact with our API endpoint as we pass the two data info email and passwords
+        signupBtn.setOnClickListener {
+
+//            Specify the API endpoint
+            val api = "http://frostyghost23.alwaysdata.net/api/signin"
+
+//            Create RequestParams that will hold the data to be sent to the API
+            val data = RequestParams()
+
+//            Add/append/attach the data to be sent to the API
+            data.put("email", email.text.toString())
+            data.put("password", password.text.toString())
+
+//            Import the API helper
+//            Application context is passed to the helper class, it tells us what to do with the data
+            val helper = ApiHelper(applicationContext)
+
+//            By use of the function post_login inside of the helper class, post your data
+            helper.post_login(api, data)
+
+
+        }
+
+        }
 }
--- app/src/main/java/com/example/sokogarden/Signup.kt
+++ app/src/main/java/com/example/sokogarden/Signup.kt
@@ -1,10 +1,15 @@
 package com.example.sokogarden
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.loopj.android.http.RequestParams
 
 class Signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,54 @@ class Signup : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+//        Find all View by use of their ids
+        val username = findViewById<EditText>(R.id.username)
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val phone = findViewById<EditText>(R.id.phone)
+
+        val signupButton = findViewById<Button>(R.id.signupBtn)
+        val signinTextView = findViewById<TextView>(R.id.signintxt)
+
+//        Below when a person clicks on the Textview, they navigate to the signin activity
+        signinTextView.setOnClickListener {
+            val intent = Intent(applicationContext, Signin::class.java)
+            startActivity(intent)
+        }
+
+//        On click of signup button, register a person
+
+        signupButton.setOnClickListener {
+//            Specify the API endpoint
+            val api = "http://frostyghost23.alwaysdata.net/api/signup"
+
+//            Create a request params to hold all data
+            val data = RequestParams()
+
+//            Add/append the username, email, password and phone to the request
+            data.put("username", username.text.toString().trim())
+            data.put("email", email.text.toString().trim())
+            data.put("password", password.text.toString().trim())
+            data.put("phone", phone.text.toString().trim())
+
+//            import the API helper
+            val helper = ApiHelper(applicationContext)
+
+//            Inside of the helper class, access the post function and pass the api endpoint and the data
+            helper.post(api, data)
+
+            email.text.clear()
+            password.text.clear()
+            username.text.clear()
+            phone.text.clear()
+
+//            intent to the main activity
+//            val intent = Intent(applicationContext, MainActivity::class.java)
+//            startActivity(intent)
+
+        }
+
+
     }
 }
